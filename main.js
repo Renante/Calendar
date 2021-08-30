48c9165b32d4a76fc74221f492b7b83669bc8b94
Renante <renanteabril@gmail.com>
2021-08-31 00:50:39 +0800

    Feature/cleanup (#1) (#2)
    * updated demo
    
    * added readme file
    
    * 1.0.8

--- main.js
+++ main.js
@@ -1,9 +1,22 @@
 var app = angular.module('app', ['rdaCalendar']);
-app.controller('defaultController', function ($scope) {
-   $scope.onCalendarUpdated = function(info) {
-       angular.forEach(info.days, function(day){
-           day.dayFormat = `<span>${ day.day }</span>`;
-       });
-   }
+app.controller('defaultController', function ($scope, $timeout) {
+
+    $scope.onCalendarUpdated = function (info) {
+        angular.forEach(info.days, function (day) {
+            day.dayFormat = `<span>${day.day}</span>`;
+        });
+    }
+
+    $timeout(function () {
+        $scope.$broadcast('changeDayFormat', { day: 1, dayFormat: 'first day of the month' });
+    });
+
+    $timeout(function(){
+        $scope.$broadcast('updateCalendar');
+    }, 1000);
+
+    $scope.onDaySelected = function (day) {
+        console.log(`Day selected ${day}`);
+    }
 });
 
